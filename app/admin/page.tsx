a8f6d343f02c77016c127beefc68c5685c0ab953
alinneducation <alinneducation@gmail.com>
2025-07-19 19:18:26 +0630

    google auth

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,7 +1,6 @@
 import { createClient } from "@/utils/supabase/server";
 
 export default async function AccountForm() {
-  const supabase = await createClient();
   return (
     <div>
       <h1>Admin Dashboard</h1>
