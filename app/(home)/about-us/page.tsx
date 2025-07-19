d38e31340bf8e1de98fe9da8a54bb2b5cb4ba99e
alinneducation <alinneducation@gmail.com>
2025-07-19 19:04:06 +0630

    google auth

--- app/(home)/about-us/page.tsx
+++ app/(home)/about-us/page.tsx
@@ -1,22 +1,11 @@
 "use client";
 import { useState } from "react";
-import db from "@/firebase/firestore";
-import { collection, addDoc } from "firebase/firestore";
 
 export default function Page() {
   const [value, setValue] = useState("");
 
   const handleSubmit = async (event: { preventDefault: () => void }) => {
     event.preventDefault();
-    try {
-      const docRef = await addDoc(collection(db, "items"), {
-        name: value,
-      });
-      console.log("Document written with ID: ", docRef.id);
-      setValue(""); // Clear the form
-    } catch (e) {
-      console.error("Error adding document: ", e);
-    }
   };
 
   return (
