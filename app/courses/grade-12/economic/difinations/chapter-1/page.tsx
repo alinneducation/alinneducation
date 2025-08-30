0c501294a28c0e0f4b6a69f1e664c83c89501fd9
alinneducation <alinneducation@gmail.com>
2025-08-30 21:54:57 +0630

    commit 5

--- app/courses/grade-12/economic/difinations/chapter-1/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Chapter1 } from "@/components/courses/grade-12/economic/difinations";
-
-export default function page() {
-  return <Chapter1 />;
-}
--- app/courses/grade-12/economic/difinations/chapter-4/page.tsx
+++
@@ -1,5 +0,0 @@
-import { Chapter4 } from "@/components/courses/grade-12/economic/difinations";
-
-export default function page() {
-  return <Chapter4 />;
-}
